cc1109eff0a7962a22ecc20895b52311805e6016
Jon Gutschon <jongutschon123@gmail.com>
2021-09-06 22:37:26 -0400

    Revert "restore palette cycling"
    This reverts commit e36e965d90bffa9e7b96db81c90395bd664c0f05.

--- lightController/commands.cpp
+++ lightController/commands.cpp
@@ -13,16 +13,15 @@ void lightSwitch(char* setting) {
 
 void setPalette(char* palette) {
   // placeholder to retain original functionality
-  if (strstr(palette, "next")) {
-    cyclePalette('n');
-  } else if (strstr(palette, "prev")) {
-    cyclePalette('p');
-  } 
-  // else {
-  //   Serial.print("+");
-  //   Serial.print(palette);
-  //   Serial.println("+");
-  //   currentPalette = paletteMap[palette];
+  // if (strstr(palette, "next")) {
+  //   cyclePalette('n');
+  // } else if (strstr(palette, "prev")) {
+  //   cyclePalette('p');
+  // } else {
+    Serial.print("+");
+    Serial.print(palette);
+    Serial.println("+");
+    // currentPalette = paletteMap[palette];
   // }
 }
 
